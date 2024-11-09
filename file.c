56acf270c43f3dbb617f06dfb1cbbcdc9c3dbd0b
Phil Samuel <philipmayr@outlook.com>
2024-11-09 13:10:57 -0600

    Create file.c

---
+++ file.c
@@ -0,0 +1,102 @@
+// Primitive Root Finder
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int find_binary_logarithm(int number)
+{
+    int logarithm = 1;
+    
+    while (number >>= 1) logarithm++;
+    
+    return logarithm;
+}
+
+int * find_prime_factors(int number, int upper_bound)
+{
+    int * prime_factors = malloc(sizeof (int) * upper_bound);
+    
+    int index = 0;
+    
+    while (~number & 1)
+    {
+        prime_factors[index] = 2;
+        
+        number >>= 1;
+        
+        index++;
+    }
+    
+    for (int factor_candidate = 3; factor_candidate <= sqrt(number); factor_candidate += 2)
+    {
+        while (number % factor_candidate == 0)
+        {
+            int factor = factor_candidate;
+            
+            prime_factors[index] = factor;
+            
+            number /= factor;
+            
+            index++;
+        }
+    }
+    
+    if (number > 2) prime_factors[index] = number;
+    
+    return prime_factors;
+}
+
+int find_primitive_root(int prime_modulus)
+{
+    if (prime_modulus == 2) return 1;
+    
+    // primitive root α
+    int α = 2;
+    
+    int prime_modulus_less_one = prime_modulus - 1;
+    
+    
+}
+
+int main()
+{
+    for (;;)
+    {    
+        printf("Enter a number to find its prime factors: ");
+        
+        // integer input validation
+        // https://jackstromberg.com/2013/02/how-to-validate-numeric-integer-input-in-c/
+        
+        int input, status, buffer;
+
+    	status = scanf("%d", &input);
+    	
+    	while (status != 1)
+    	{
+            while ((buffer = getchar()) != EOF && buffer != '\n');
+            
+            printf("Invalid input.");
+            printf("\n\n");
+            printf("Enter a number to find its prime factors: ");
+            
+            status = scanf("%d", &input);
+    	}
+    
+    	int number = input;
+    
+        int upper_bound = find_binary_logarithm(number);
+        
+        int * prime_factors = find_prime_factors(number, upper_bound);
+        
+        for (int index = 0; index < upper_bound; index++)
+        {
+            if (* (prime_factors + index) == 0) break;
+            printf("%d ", * (prime_factors + index));
+        }
+            
+            printf("\n\n");
+        }
+    
+    return 0;
+}
